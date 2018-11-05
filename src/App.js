90cb5b6a70ebd2d5cb20a89d8c025427d745f6fc
Huan Zhang <huan.zhang@trinitymirror.com>
2018-11-05 19:47:00 +0000

    test web3 methods

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Web3 from 'web3';
 import './App.css';
 
 class App extends Component {
   render() {
+    var web3 = new Web3(Web3.givenProvider)
+    console.log(web3)
+    web3.eth.getBlock("latest", true).then((res) => {
+      console.log(res)
+      res.transactions.forEach(transaction => {
+        let value = transaction.value
+        console.log(web3.utils.fromWei(web3.utils.hexToNumberString(value)))
+      })
+
+    })
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Block Explorer</h2>
         </header>
       </div>
     );
